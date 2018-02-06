5309bdec7d73609a2f7d77682f30076ae5fd86c5
«IzhevskiyYuriy» <juriyy-ijevskiyy@rambler.ru>
2018-02-06 18:07:27 +0200

    add project

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tz.loc/upload/admin/');
+define('HTTP_CATALOG', 'http://tz.loc/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://tz.loc/upload/admin/');
+define('HTTPS_CATALOG', 'http://tz.loc/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/tz.loc/upload/admin/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/tz.loc/upload/system/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/tz.loc/upload/admin/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/tz.loc/upload/admin/view/template/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/tz.loc/upload/system/config/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/tz.loc/upload/image/');
+define('DIR_CACHE', 'D:/OSPanel/domains/tz.loc/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/tz.loc/upload/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/tz.loc/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/tz.loc/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/tz.loc/upload/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OSPanel/domains/tz.loc/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sandi');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tz.loc/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://tz.loc/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/tz.loc/upload/catalog/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/tz.loc/upload/system/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/tz.loc/upload/catalog/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/tz.loc/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/tz.loc/upload/system/config/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/tz.loc/upload/image/');
+define('DIR_CACHE', 'D:/OSPanel/domains/tz.loc/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/tz.loc/upload/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/tz.loc/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/tz.loc/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/tz.loc/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sandi');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
